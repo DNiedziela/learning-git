f3ff645ae5da08403bb465b4fa1ea63d03b0745a
Dawid Niedziela <dniedziela97@gmail.com>
2025-05-08 10:48:18 +0200

    add ignore-symbols code

--- palindrom zadanie/palindrom.py
+++ palindrom zadanie/palindrom.py
@@ -1,5 +1,15 @@
+import string
+
 def is_palindrome(word):
-    return word == word[::-1]
+    word = word.lower()
+    letters_only = ''.join(char for char in word if char in string.ascii_lowercase)
+    return letters_only == letters_only[::-1]
+    
+  
+
 
 print(is_palindrome("radar"))
-print(is_palindrome("bulbulator")) 
+print(is_palindrome("bulbula@Eto@#346 r")) 
+print(is_palindrome("A man, a plan, a canal - panama"))
+print(is_palindrome("Definitely a palindrome")) 
+print(is_palindrome("B")) 
