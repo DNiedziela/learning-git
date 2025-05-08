235b306af85d1b456bec0c528087e35dd90defec
Dawid Niedziela <dniedziela97@gmail.com>
2025-05-08 11:11:44 +0200

    Add isalnum instead of ascii

--- palindrom zadanie/palindrom.py
+++ palindrom zadanie/palindrom.py
@@ -1,15 +1,13 @@
-import string
-
 def is_palindrome(word):
     word = word.lower()
-    letters_only = ''.join(char for char in word if char in string.ascii_lowercase)
+    letters_only = ''.join(char for char in word if char.isalpha())
     return letters_only == letters_only[::-1]
     
   
 
 
 print(is_palindrome("radar"))
-print(is_palindrome("bulbula@Eto@#346 r")) 
+print(is_palindrome("Kobyća ma mały bok")) 
 print(is_palindrome("A man, a plan, a canal - panama"))
 print(is_palindrome("Definitely a palindrome")) 
-print(is_palindrome("B")) 
+print(is_palindrome("Łał")) 
